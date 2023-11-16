fa69c8cf746c1b60f7e7090f252c36f9d7756a62
chrisbekk <christianbekkelund@gmail.com>
2023-11-16 16:45:10 +0100

    Installed GSAP and added text animation to index.html

---
+++ src/js/animations/title.js
@@ -0,0 +1,18 @@
+import {gsap} from "gsap";
+import { TextPlugin } from "gsap/TextPlugin";
+gsap.registerPlugin(TextPlugin)
+
+export function animateTitle(){
+    
+    const title = document.querySelector("#hero-title")
+    gsap.to(title, {
+        duration: 2.5,
+        text: {
+            value: "We Bring Good Deals To You",
+            delimiter: "",
+            padSpace: true
+        },
+        ease: "none",
+        delay: 2
+    });
+}
--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,7 @@
 // Import our custom CSS
 import '../scss/styles.scss'
-
+import { animateTitle } from './animations/title'
 // Import all of Bootstrap's JS
 import * as bootstrap from 'bootstrap'
+
+animateTitle()
