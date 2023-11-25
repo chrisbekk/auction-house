e95695dd7f3169fb1a5a9c0d5d810ce933ab90e0
chrisbekk <christianbekkelund@gmail.com>
2023-11-25 15:52:29 +0100

    minor fix

--- src/profile/profile.js
+++ src/profile/profile.js
@@ -1,4 +1,4 @@
 import "../scss/styles.scss";
-import { checkState } from "./state/user.js";
+import { checkState } from "../js/state/user.js";
 // Import all of Bootstrap's JS
 import * as bootstrap from "bootstrap";
